c1c4d3134652e7299761fd6fc5feb614a6fdb376
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2019-03-24 12:17:38 +0100

    Tests now working locally

---
+++ tests/context.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+print(os.path.dirname(__file__))
+
+#for p in sys.path:
+#    print(p)
+
+import toypy
