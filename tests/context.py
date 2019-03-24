1dbee830d1fd952cbd4fa01f2baabbe787bd7019
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2019-03-24 12:27:04 +0100

    Removed unneccesary lines

--- tests/context.py
+++ tests/context.py
@@ -4,9 +4,4 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-print(os.path.dirname(__file__))
-
-#for p in sys.path:
-#    print(p)
-
 import toypy
