36dc90b0b4edcbad68599048ddea6c7f1e34f6cd
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2019-03-24 11:38:22 +0100

    Added setup and license

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/PabRod/toypy
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='toypy',
+    version='0.1.0',
+    description='Toy package',
+    long_description=readme,
+    author='Pablo Rodríguez-Sánchez',
+    author_email='pablo.rodriguez.sanchez@gmail.com',
+    url='https://github.com/PabRod/toypy',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
