c6e34a998e4d8fa640a0e4406a2d680db0e51762
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2019-03-25 13:37:33 +0100

    Improved test
    Now it checks (input, expected output) tuples

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -2,8 +2,19 @@ from tools import *
 import pytest
 
 
-@pytest.mark.parametrize('x_deg', [0, 30, 45, 60, 90])
-def test_sindeg(x_deg):
-    f = sindeg(x_deg)
-    assert(np.abs(f) <= 1), \
-        'sindeg should be bounded between -1 and 1'
+# @pytest.mark.parametrize('x_deg', [0, 30, 45, 60, 90])
+
+@pytest.mark.parametrize("input, exp_output", [
+    (0, 0),
+    (30, 1/2),
+    (45, np.sqrt(2)/2),
+    (60, np.sqrt(3)/2),
+    (90, 1),
+    (180, 0),
+    (270, -1)
+])
+def test_sindeg(input, exp_output):
+    tol = 1e-6
+    f = sindeg(input)
+    assert(f == pytest.approx(exp_output, tol)), \
+        'sindeg is not behaving as expected'
